41fbd8b253a3218ccfd1e8eaf93b3cadc52946c7
nizamike229 <nizamike229@mail.ru>
2025-05-31 17:52:17 +0500

    Added default starting

---
+++ tgBot/tgBot/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using tgBot;
+using Telegram.Bot;
+
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+    {
+        BotBusinessLogic.ConfigureServices(services);
+        services.AddSingleton<ITelegramBotClient>(new TelegramBotClient("7412325055:AAEmva3flztRdn0iXKkSg__qJGEpnjoiC98"));
+        services.AddHostedService<TelegramBotHostedService>();
+    });
+
+await builder.RunConsoleAsync();
